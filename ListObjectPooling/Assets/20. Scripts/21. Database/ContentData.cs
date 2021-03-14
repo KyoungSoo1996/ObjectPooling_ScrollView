59f1e5993960828851c863e8c885211b8dcd759d
JeoungKyoungSoo <wjdrudtn225@gmail.com>
2021-03-15 08:49:24 +0900

    Init
    Init

---
+++ ListObjectPooling/Assets/20. Scripts/21. Database/ContentData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class ContentData
+{
+    public int number { get; set; }
+    public UnityEngine.Color color { get; set; }
+    public Action buttonEvent { get; set; }
+
+    public ContentData(int _number, UnityEngine.Color _color, Action _buttonEvent)
+    {
+        this.number = _number;
+        this.color = _color;
+        this.buttonEvent = _buttonEvent;
+    }
+}
---
+++ ListObjectPooling/Assets/20. Scripts/contentControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class contentControl : MonoBehaviour
+{
+    [SerializeField] Text text_number;
+    [SerializeField] Image image_colorView;
+    [SerializeField] Button btn_debugPrinter;
+    [SerializeField] CanvasGroup canvasGroup;
+
+    public void SetData(ContentData _contentData)
+    {
+        canvasGroup.alpha = 1;
+        canvasGroup.blocksRaycasts = true;
+        text_number.text = _contentData.number.ToString();
+        btn_debugPrinter.onClick.AddListener(delgate {btn_debugPrinter()});
+    }
+
+    public void InitData()
+    {
+        canvasGroup.alpha = 0;
+        canvasGroup.blocksRaycasts = false;
+    }
+}
